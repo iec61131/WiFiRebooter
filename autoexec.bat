23db23848f649f2d333120e3764dd867f44b52ea
iec61131 <58704008+iec61131@users.noreply.github.com>
2023-12-07 19:14:44 -0500

    Add files via upload
    [Added] example files

---
+++ autoexec.bat
@@ -0,0 +1,8 @@
+; how to delete rule http://10.42.0.76/ufsd?delete=/autoexec.bat&download=/
+; tested with Tasmota 13.2.0.3(tasmota-4M)
+; ensure relay on after reboot
+PowerOnState 1
+SetOption36 0
+Rule1 1
+Rule2 1
+; eof
---
+++ formatted_rules_with_explanation.bat
@@ -0,0 +1,17 @@
+;# bugfix, power1 boot trigger https://world.hey.com/goekesmi/using-tasmota-without-a-network-a-post-preserved-from-the-past-303b26f0
+;# improved example based on https://tasmota.github.io/docs/Rules/#watchdog-for-wi-fi-router-or-modem
+
+;# Var1 => ping time interval in minutes ( default 1min)
+;# Var2 => Counter for Google DNS (8.8.8.8) not reachable or wifi down
+;# Var3 => last timestamp of outage caused by internet/ISP down
+;# Var4 => inital boot up wait time for Wifi / modem to be ready or after forced wifi restart. Default 10min. (var4 >= var1)
+;# Var5 => current interval time in minutes culculated at runtime ( default set to same value as var4)
+
+;# set unix time to 2023/01/01 and enables timer mechanism to work for rules
+Rule1 ON Power1#Boot Do Time 1672531201 ENDON
+
+Rule2
+ON Power1#Boot do backlog Var1 1; Var2 0; Var3 0; Var4 10; Var5 10; ENDON
+ON Time#Minute|%Var5% DO backlog LedState 0; Var; Delay 100; Ping4 8.8.8.8; ENDON
+ON Ping#8.8.8.8#Success==0 DO backlog Var5 %Var4%; Power1 0; Delay 40; Power1 1; Add2 1; Var3 %timestamp%; ENDON
+ON Ping#8.8.8.8#Success>0 DO backlog LedState 1; Var5 %Var1%; ENDON
